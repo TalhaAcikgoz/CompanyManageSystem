8377ff156b1db60c346bfe9c3d34fa392c9fbe78
TalhaAcikgoz <89697506+TalhaAcikgoz@users.noreply.github.com>
2024-08-19 21:53:50 +0300

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyIdentityApp.Data;
+
+
+public class ApplicationUser : IdentityUser
+{
+    public string? FullName { get; set; }
+}
+public class AccountController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+}
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyIdentityApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/* ==================== IDENTITY CONFIGURATION ==================== */
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddRazorPages();
+/* ==================== ==================== */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
